2654dbba852d5fba2409d2c7989d9414c7d83fd3
GregFee <36000912+GregFee@users.noreply.github.com>
2018-04-13 11:17:05 +0100

    Update iris.py

--- iris.py
+++ iris.py
@@ -2,17 +2,16 @@
 #https://en.wikipedia.org/wiki/Iris_flower_data_set
 
 #import libraries
-import numpy 
-import pandas as pd 
+import numpy as np
+import scipy
+import pandas as pd
 import matplotlib.pyplot as plt
 
-#import data from address
-url = "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
-names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
-iris = pd.read_csv(url, names=names)
+#read data file in numpy array
+iris = np.genfromtxt ('data/iris.csv', delimiter=",")
 
-#print data for checking
-print(iris)
+
+
 
 
 
